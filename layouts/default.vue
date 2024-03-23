98d2f35ef0982ed7bdfe64cad1e468f7de27cbc7
Hyosuporte <cacuervo120@gmail.com>
2024-03-22 19:59:54 -0500

    feat: init proyect nuxt-vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        
+        <slot></slot>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+main {
+    background: linear-gradient(180deg, black, #2B4353);
+    height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <Navbar />
+    <div>
+        <h1>Page index</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
